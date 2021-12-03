05075d030f8335580711922bba2162ef918a3bf0
Amrita Matta <matta.amrita@gmail.com>
2021-12-03 15:08:32 +0200

    added css for explore collection

--- src/components/exploreitems/ExploreourCollection.js
+++ src/components/exploreitems/ExploreourCollection.js
@@ -1,43 +1,45 @@
-import React, { Component } from "react";
-import { items } from "./clothesdata";
+import React, {Component} from "react";
+import {items} from "./clothesdata";
 import SingleItem from "./SingleItem";
-import { Switch, Route } from "react-router";
+import {Switch, Route} from "react-router";
 import ProductDetail from "./ProductDetail";
 
-class ExploreourCollection extends Component {
-  state = {
-    searchInput: "",
-  };
-  searchInputHandler = (event) => {
-    this.setState({
-      searchInput: event.target.value,
-    });
-  };
-  itemsListing = items.map((item) => <SingleItem key={item.name} {...item} />);
-  render() {
-    const itemsFilter = items.filter((item) => {
-      return item.name
-        .toLowerCase()
-        .includes(this.state.searchInput.toLowerCase());
-    });
-    const itemsListing = itemsFilter.map((item) => (
-      <SingleItem key={item.name} {...item} />
-    ));
-    return (
-      <Switch>
-        <Route exact path={this.props.match.path}>
-          <div className="searchInput">
-            <h2>Search: </h2>
-            <input type="text" onChange={this.searchInputHandler}></input>
-          </div>
-          {itemsListing}
-        </Route>
-        <Route path={`${this.props.match.path}/:product`}>
-          <ProductDetail />
-        </Route>
-      </Switch>
-    );
-  }
+class ExploreOurCollection extends Component {
+    state = {
+        searchInput: "",
+    };
+    searchInputHandler = (event) => {
+        this.setState({
+            searchInput: event.target.value,
+        });
+    };
+    itemsListing = items.map((item) => <SingleItem key={item.name} {...item} />);
+
+
+    render() {
+        const itemsFilter = items.filter((item) => {
+            return item.name
+                .toLowerCase()
+                .includes(this.state.searchInput.toLowerCase());
+        });
+        const itemsListing = itemsFilter.map((item) => (
+            <SingleItem key={item.name} {...item} />
+        ));
+        return (
+            <Switch>
+                <Route exact path={this.props.match.path}>
+                    <div className="searchInput">
+                        <h2>Search: </h2>
+                        <input type="text" onChange={this.searchInputHandler}></input>
+                    </div>
+                    {itemsListing}
+                </Route>
+                <Route path={`${this.props.match.path}/:product`}>
+                    <ProductDetail/>
+                </Route>
+            </Switch>
+        );
+    }
 }
 
-export default ExploreourCollection;
+export default ExploreOurCollection;
--- src/components/exploreitems/SingleItem.js
+++ src/components/exploreitems/SingleItem.js
@@ -1,34 +1,32 @@
-import React, { useContext } from "react";
-import { Link, useRouteMatch } from "react-router-dom";
+import React, {useContext} from "react";
+import {Link, useRouteMatch} from "react-router-dom";
 import "./SingleItem.css";
-import { AppContext } from "../../Context";
+import {AppContext} from "../../Context";
 
 const SingleItem = (props) => {
-  const match = useRouteMatch();
-  const [itemsInCart, setItemsInCart] = useContext(AppContext);
+    const match = useRouteMatch();
+    const [itemsInCart, setItemsInCart] = useContext(AppContext);
 
-  console.log(itemsInCart);
-
-  return (
-    <div className="itemCard">
-      <Link to={`${match.url}/${props.name}`}>
-      <h2>{props.name}</h2>
-      <img className="small" src={props.image} alt="product_picture" />
-      <p>{props.price}</p>
-      
-      <button
-        disabled={itemsInCart.some((item) => item.id === props.id)}
-        className="add_cart_btn"
-        onClick={() => {
-          setItemsInCart([...itemsInCart, props]);
-        }}
-      >
-        {" "}
-        Add to Cart{" "}
-      </button>
-      </Link>
-    </div>
-  );
+    console.log(itemsInCart);
+    return (
+        <div className="itemsCard">
+            <Link to={`${match.url}/${props.name}`}>
+                <h2>{props.name}</h2>
+                <img src={props.image} alt="product_picture"/>
+                <p>{props.price}</p>
+                <Link to={`${match.url}/${props.name}`}>Read more</Link>
+                <button
+                    disabled={itemsInCart.some((item) => item.id === props.id)}
+                    className="add_cart_btn"
+                    onClick={() => {
+                        setItemsInCart([...itemsInCart, props]);
+                    }}
+                >
+                Add to Cart
+                </button>
+            </Link>
+        </div>
+    );
 };
 
 export default SingleItem;
