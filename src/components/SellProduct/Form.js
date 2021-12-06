b0ba9bd950827e00f588eb17371dc47c5cdd0208
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2021-12-06 13:14:26 +0200

    css for selling page

--- src/components/SellProduct/Form.js
+++ src/components/SellProduct/Form.js
@@ -1,135 +1,140 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import PublishPost from "./Publish your post.svg";
-import preview from "./preview.svg";
-import publish from "./publish.svg";
 import sellerInfo from "./SellerInfo.svg";
+import close from "./close.svg";
 
 const Form = (props) => {
   const CloseHandler = () => {
     window.location.reload();
   };
+
+  const history = useHistory();
+
   return (
     <form onSubmit={CloseHandler}>
-      <div className="publishYourPost">
-        <img src={PublishPost} alt="" />
-        <div className="form__textBox">
-          <label htmlFor="categories"> </label>
-          <select
-            className="form_textBox"
-            name="categories"
-            id="categories"
-            placeholder="Choose a category"
-            required
-          >
-            <option value="None">Choose a category</option>
-            <option value="Clothing">Clothing</option>
-            <option value="Bags">Bags</option>
-            <option value="Other">Other</option>
-          </select>
-        </div>
-        <div className="form__textBox">
-          <label htmlFor="heading"> </label>
-          <input
-            type="text"
-            className="form_textBox"
-            name="heading"
-            id="heading"
-            placeholder="Heading"
-            required
-          />
-        </div>
-        <div className="form__textBox">
-          <label htmlFor="description"> </label>
-          <textarea
-            rows="6"
-            cols="47"
-            name="description"
-            id="description"
-            placeholder="Product Description"
-            className="form_textBox"
-          ></textarea>
-        </div>
-        <div className="form__textBox">
-          <label htmlFor="price"></label>
-          <input
-            type="number"
-            name="price"
-            id="price"
-            placeholder="Price"
-            className="form_textBox"
-          />
-        </div>
-        <div className="form__textBox">
-          <label htmlFor="avatar">Upload a Picture </label>
-          <input
-            type="file"
-            id="avatar"
-            name="avatar"
-            accept="image/png, image/jpeg"
-            className="form_textBox"
-            multiple
-          />
+      <div className="form-container">
+        <img
+          className="close"
+          src={close}
+          alt=""
+          onClick={() => history.goBack()}
+        />
+        <div className="publishYourPost">
+          <img src={PublishPost} alt="" />
+          <div className="form__textBox">
+            <label htmlFor="categories"> </label>
+            <select
+              className="form_textBox"
+              name="categories"
+              id="categories"
+              placeholder="Choose a category"
+              required
+            >
+              <option value="None">Choose a category</option>
+              <option value="Clothing">Clothing</option>
+              <option value="Bags">Bags</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+          <div className="form__textBox">
+            <label htmlFor="heading"> </label>
+            <input
+              type="text"
+              className="form_textBox"
+              name="heading"
+              id="heading"
+              placeholder="Heading"
+              required
+            />
+          </div>
+          <div className="form__textBox">
+            <label htmlFor="description"> </label>
+            <textarea
+              rows="6"
+              cols="47"
+              name="description"
+              id="description"
+              placeholder="Product Description"
+              className="form_textBox"
+            ></textarea>
+          </div>
+          <div className="form__textBox">
+            <label htmlFor="price"></label>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              placeholder="Price"
+              className="form_textBox"
+            />
+          </div>
+          <div className=" upload">
+            <label htmlFor="avatar">Upload a Picture </label>
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              accept="image/png, image/jpeg"
+              className="form_textBox"
+              multiple
+            />
+          </div>
+
+          <div className="form__textBox">
+            <label htmlFor="location"> </label>
+            <input
+              type="text"
+              name="location"
+              id="location"
+              placeholder="Location"
+              className="form_textBox"
+              required
+            />
+          </div>
         </div>
 
-        <div className="form__textBox">
-          <label htmlFor="location"> </label>
-          <input
-            type="text"
-            name="location"
-            id="location"
-            placeholder="Location"
-            className="form_textBox"
-            required
-          />
+        <div className="sellerInfomation">
+          <img src={sellerInfo} alt="" />
+          <div className="form__textBox">
+            <label></label>
+            <input
+              type="text"
+              maxLength="25"
+              placeholder="Name"
+              className="form_textBox"
+            />
+          </div>
+          <div className="form__textBox">
+            <label></label>
+            <input
+              type="email"
+              className="email"
+              placeholder="Email"
+              className="form_textBox"
+              readOnly
+            />
+          </div>
+          <div className="form__textBox">
+            <label></label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              className="form_textBox"
+              placeholder="Phone number"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+              required
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="sellerInfomation">
-        <img src={sellerInfo} alt="" />
-        <div className="form__textBox">
-          <label></label>
-          <input
-            type="text"
-            maxLength="25"
-            placeholder="Name"
-            className="form_textBox"
-          />
-        </div>
-        <div className="form__textBox">
-          <label></label>
-          <input
-            type="email"
-            className="email"
-            placeholder="Email"
-            className="form_textBox"
-            readOnly
-          />
-        </div>
-        <div className="form__textBox">
-          <label></label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            className="form_textBox"
-            placeholder="Phone number"
-            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-            required
-          />
-        </div>
-      </div>
+        <div className="buttons">
+          <input type="submit" value="Publish" className="submit" />
 
-      <div className="buttons">
-        <img className="submit" src={preview} alt="" />
-        <img className="preview" src={publish} alt="" />
+          <input type="button" value="Preview" className="preview" />
+        </div>
       </div>
-
-      {/* <div className="submit">
-        <input type="submit" value="Publish" className="submit" />
-      </div>
-      <div className="preview">
-        <input type="button" value="Preview" className="preview" />
-      </div> */}
     </form>
   );
 };
