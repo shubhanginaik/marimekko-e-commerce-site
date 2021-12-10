5c124a04d7647a0c64bfedd65b2bcd0d8b03256b
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2021-12-10 14:20:50 +0200

    fixing footer css and adding function for join-us button

--- src/components/pageComponents/footer/Footer.js
+++ src/components/pageComponents/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import joinButton from "./join-btn.svg";
 import footer from "./footer.svg";
 import "./Footer.css";
@@ -7,7 +8,10 @@ const Footer = () => {
   return (
     <div className="footer-section">
       <img className="footer" src={footer} alt="" />
+      
+      <Link to="/login">
       <img className="join-btn" src={joinButton} alt="" />
+      </Link>
     </div>
   );
 };
