3b7a7e8a64d8a0cd8b120fb9d5371ce9d205682b
BC-macair032 <bc-macair032@BC-macair032s-MacBook-Air.local>
2022-04-02 21:14:04 +0300

    previous and next button functionality and search functionality added

--- src/components/exploreitems/BuyWithFirebase.js
+++ src/components/exploreitems/BuyWithFirebase.js
@@ -7,6 +7,7 @@ import ProductDetail from "./ProductDetail";
 function BuyWithFirebase()  {
     const [products,setProducts]=useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [searchValue, setSearchValue] = useState("");
     const match = useRouteMatch();
     const fetchProducts=async()=>{
         const response=db.collection('sellcontact');
@@ -24,17 +25,34 @@ function BuyWithFirebase()  {
         fetchProducts();
         console.log('products',products);
       }, []);
-      const recentList=products.slice(0,12);
+      
+      
+
+      const searchHandler = (e) => {
+        setSearchValue(e.target.value);
+        console.log('reached',searchValue);
+        searchValue === ''
+          ? setProducts(products)
+          : setProducts(
+            products.filter((product) => product.heading.includes(searchValue))
+            );
+      };
+    
     return (
         <Switch>
         <Route  exact path={`${match.path}`}>
-        <div>
+        <div className="explore-our-collection">
+                    <div className="searchInput">
+                    <input type="text"  className="search"  placeholder="Search" onChange={e => setSearchValue(e.target.value)} />
+                    </div>
+                    <div className="product-page">
            {
            
            !isLoading &&
-           recentList && recentList.map((product)=>(<SingleItem key={product.id} {...product} />)
+           products && products.filter(li => li.heading.toLowerCase().includes(searchValue.toLowerCase())).map((product)=>(<SingleItem key={product.id} {...product} />)
            )}
         </div>
+                    </div>
         </Route>
                 <Route path={`${match.path}/:id`}>
                     <ProductDetail/>
